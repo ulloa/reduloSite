3468500f2b8c087066fe10281119def527bf132f
Samuel Ulloa <samuel.ulloa@gmail.com>
2019-11-08 00:05:20 -0800

    Setup of structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.jsx';
+import App from './containers/App/index.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
